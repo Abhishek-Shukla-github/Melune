d37d090481d4e1d0e1d6ff4e42ead95a088ec2e5
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2025-02-09 22:52:57 -0500

    Complete Auth Setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 function App() {
 
   return (
-    <h1 className="text-red-400">Hare Krishna!</h1>
+    <>
+      <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      <h1 className="text-red-400">Hare Krishna!</h1>
+    </>
   )
 }
 
