6f313f6393394f0d8b22713654df440fbd3f22b1
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2025-02-10 22:01:00 -0500

    Complete Admin routes and controllers

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,9 +1,18 @@
-import {Router} from "express";
+import { Router } from "express";
+import { checkAdmin, createAlbum, createSong, deleteAlbum, deleteSong } from "../controller/admin.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", (req,res) => {
-    res.send("Admin Router get route")
-})
+//Authenticate upfront if the user is admin
+router.use(protectRoute, requireAdmin);
+
+router.get("/check", checkAdmin);
+
+router.post("/songs", createSong);
+router.delete("/songs/:id", deleteSong);
+
+router.post("/albums", createAlbum);
+router.delete("/albums/:id", deleteAlbum);
 
 export default router;
