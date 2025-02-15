b19ed7c92e8ec01f847d4005c23a8cef49dfbbd6
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2025-02-15 12:04:02 -0500

    Prepare Deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,11 @@ import { clerkMiddleware } from "@clerk/express";
 import fileUpload from "express-fileupload";
 import path from "path";
 import cors from "cors";
+import fs from "fs";
+import { createServer } from "http";
+import cron from "node-cron";
+
+import { initializeSocket } from "./lib/socket.js";
 
 import { connectDB } from "./lib/db.js";
 import userRoutes from "./routes/user.route.js";
@@ -12,17 +17,15 @@ import authRoutes from "./routes/auth.route.js";
 import songRoutes from "./routes/song.route.js";
 import albumRoutes from "./routes/album.route.js";
 import statRoutes from "./routes/stat.route.js";
-import { createServer } from "http";
-import { initializeSocket } from "./lib/socket.js";
 
 dotenv.config();
-
 
 const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT;
+
 const httpServer = createServer(app);
-initializeSocket(httpServer)
+initializeSocket(httpServer);
 
 app.use(
 	cors({
@@ -43,7 +46,22 @@ app.use(
 		},
 	})
 );
-;
+
+// cron jobs
+const tempDir = path.join(process.cwd(), "tmp");
+cron.schedule("0 * * * *", () => {
+	if (fs.existsSync(tempDir)) {
+		fs.readdir(tempDir, (err, files) => {
+			if (err) {
+				console.log("error", err);
+				return;
+			}
+			for (const file of files) {
+				fs.unlink(path.join(tempDir, file), (err) => {});
+			}
+		});
+	}
+});
 
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
@@ -65,6 +83,6 @@ app.use((err, req, res, next) => {
 });
 
 httpServer.listen(PORT, () => {
-    console.log("Server is running on PORT: ",PORT)
-    connectDB()
-})
+	console.log("Server is running on port " + PORT);
+	connectDB();
+});
